d5ad43f74095da06d8bd50546be01d5a1c3d7bbd
Brian <pacebrian0@gmail.com>
2023-10-23 13:41:53 +0200

    initial folders for server and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Container, AppBar, Typography, Grow, Grid} from '@mui/material';
+import memories from './images/memories.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Container maxwidth='lg'>
+            <AppBar position="static" color='inherit'>
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60" />
+            </AppBar>
+        </Container>
+    );
 
+};
 export default App;
